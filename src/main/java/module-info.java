28cb17c0c8c4aa79ab4bec9668584afc6a0d1ae0
Isuru N Liyanaarachchi <isurunaveen27@gmail.com>
2024-11-08 18:52:40 +0530

    reset

--- src/main/java/module-info.java
+++
@@ -1,8 +0,0 @@
-module com.example.boardingexpensetracker {
-    requires javafx.controls;
-    requires javafx.fxml;
-
-
-    opens com.example.boardingexpensetracker to javafx.fxml;
-    exports com.example.boardingexpensetracker;
-}

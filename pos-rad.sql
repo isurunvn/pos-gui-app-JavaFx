13c9ad97fd86aa638dbece617546ec8a93e3b8ba
Kavinda Dewmith <kavindadewmith@gmail.com>
2024-11-08 20:25:57 +0530

    sql file added

---
+++ pos-rad.sql
@@ -0,0 +1,324 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Nov 08, 2024 at 03:54 PM
+-- Server version: 10.4.32-MariaDB
+-- PHP Version: 8.0.30
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `pos-rad`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `admin`
+--
+
+CREATE TABLE `admin` (
+  `AdminID` int(11) NOT NULL,
+  `AdminName` varchar(100) NOT NULL,
+  `Password` varchar(100) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `admin`
+--
+
+INSERT INTO `admin` (`AdminID`, `AdminName`, `Password`) VALUES
+(1, 'AdminDoe', 'password123'),
+(2, 'SteveSmith', 'admin456'),
+(3, 'MitchellJohnson', 'superuser789'),
+(4, 'admin', 'admin'),
+(5, 'qq', 'qq');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `cashier`
+--
+
+CREATE TABLE `cashier` (
+  `CashierID` int(11) NOT NULL,
+  `CashierName` varchar(100) NOT NULL,
+  `Password` varchar(100) NOT NULL,
+  `ShopID` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `cashier`
+--
+
+INSERT INTO `cashier` (`CashierID`, `CashierName`, `Password`, `ShopID`) VALUES
+(1, 'johnsmith', 'hashed_password123', 1),
+(2, 'janedoe', 'hashed_password456', 1),
+(3, 'mikejohnson', 'hashed_password789', 2),
+(4, 'cashier', 'cashier', 2),
+(5, 'janith', 'janith', 2),
+(6, 'aa', 'aa', 1);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `order`
+--
+
+CREATE TABLE `order` (
+  `OrderID` int(11) NOT NULL,
+  `CashierID` int(11) DEFAULT NULL,
+  `Total` decimal(10,2) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `order`
+--
+
+INSERT INTO `order` (`OrderID`, `CashierID`, `Total`) VALUES
+(1, 1, 14.97),
+(2, 1, 8.98),
+(3, 2, 11.97),
+(4, 1, 1460.00),
+(5, 1, 31.86),
+(6, 1, 21.45),
+(7, 1, 29.88),
+(8, 1, 14.97),
+(9, 1, 19.42),
+(10, 1, 751.02);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `orderdetails`
+--
+
+CREATE TABLE `orderdetails` (
+  `OrderID` int(11) NOT NULL,
+  `ProductID` int(11) NOT NULL,
+  `Quantity` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `orderdetails`
+--
+
+INSERT INTO `orderdetails` (`OrderID`, `ProductID`, `Quantity`) VALUES
+(1, 1, 2),
+(1, 2, 1),
+(2, 3, 2),
+(2, 4, 2),
+(3, 2, 1),
+(3, 3, 2),
+(4, 6, 4),
+(5, 4, 8),
+(5, 5, 6),
+(6, 2, 2),
+(6, 4, 3),
+(7, 4, 7),
+(8, 1, 2),
+(8, 2, 1),
+(9, 3, 1),
+(9, 4, 7),
+(10, 2, 85),
+(10, 4, 63);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `product`
+--
+
+CREATE TABLE `product` (
+  `ProductID` int(11) NOT NULL,
+  `ProductName` varchar(100) NOT NULL,
+  `Price` decimal(10,2) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `product`
+--
+
+INSERT INTO `product` (`ProductID`, `ProductName`, `Price`) VALUES
+(1, 'Coffee', 3.99),
+(2, 'Sandwich', 6.99),
+(3, 'Cookie', 1.99),
+(4, 'Soda', 2.49),
+(5, 'Chips', 1.99),
+(6, 'Bonchi', 365.00);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `shop`
+--
+
+CREATE TABLE `shop` (
+  `ShopID` int(11) NOT NULL,
+  `ShopName` varchar(100) NOT NULL,
+  `Address` varchar(200) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `shop`
+--
+
+INSERT INTO `shop` (`ShopID`, `ShopName`, `Address`) VALUES
+(1, 'Downtown Store', '123 Main St'),
+(2, 'Mall Branch', '456 Mall Ave');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `stock`
+--
+
+CREATE TABLE `stock` (
+  `ShopID` int(11) NOT NULL,
+  `ProductID` int(11) NOT NULL,
+  `Quantity` int(11) NOT NULL DEFAULT 0
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `stock`
+--
+
+INSERT INTO `stock` (`ShopID`, `ProductID`, `Quantity`) VALUES
+(1, 1, 100),
+(1, 2, 50),
+(1, 3, 150),
+(2, 1, 80),
+(2, 4, 200),
+(2, 5, 100);
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `admin`
+--
+ALTER TABLE `admin`
+  ADD PRIMARY KEY (`AdminID`),
+  ADD UNIQUE KEY `AdminName` (`AdminName`);
+
+--
+-- Indexes for table `cashier`
+--
+ALTER TABLE `cashier`
+  ADD PRIMARY KEY (`CashierID`),
+  ADD UNIQUE KEY `Username` (`CashierName`),
+  ADD KEY `ShopID` (`ShopID`);
+
+--
+-- Indexes for table `order`
+--
+ALTER TABLE `order`
+  ADD PRIMARY KEY (`OrderID`),
+  ADD KEY `CashierID` (`CashierID`);
+
+--
+-- Indexes for table `orderdetails`
+--
+ALTER TABLE `orderdetails`
+  ADD PRIMARY KEY (`OrderID`,`ProductID`),
+  ADD KEY `ProductID` (`ProductID`);
+
+--
+-- Indexes for table `product`
+--
+ALTER TABLE `product`
+  ADD PRIMARY KEY (`ProductID`),
+  ADD UNIQUE KEY `ProductName` (`ProductName`);
+
+--
+-- Indexes for table `shop`
+--
+ALTER TABLE `shop`
+  ADD PRIMARY KEY (`ShopID`);
+
+--
+-- Indexes for table `stock`
+--
+ALTER TABLE `stock`
+  ADD PRIMARY KEY (`ShopID`,`ProductID`),
+  ADD KEY `ProductID` (`ProductID`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `admin`
+--
+ALTER TABLE `admin`
+  MODIFY `AdminID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+
+--
+-- AUTO_INCREMENT for table `cashier`
+--
+ALTER TABLE `cashier`
+  MODIFY `CashierID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+
+--
+-- AUTO_INCREMENT for table `order`
+--
+ALTER TABLE `order`
+  MODIFY `OrderID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
+
+--
+-- AUTO_INCREMENT for table `product`
+--
+ALTER TABLE `product`
+  MODIFY `ProductID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+
+--
+-- AUTO_INCREMENT for table `shop`
+--
+ALTER TABLE `shop`
+  MODIFY `ShopID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `cashier`
+--
+ALTER TABLE `cashier`
+  ADD CONSTRAINT `cashier_ibfk_1` FOREIGN KEY (`ShopID`) REFERENCES `shop` (`ShopID`);
+
+--
+-- Constraints for table `order`
+--
+ALTER TABLE `order`
+  ADD CONSTRAINT `order_ibfk_1` FOREIGN KEY (`CashierID`) REFERENCES `cashier` (`CashierID`);
+
+--
+-- Constraints for table `orderdetails`
+--
+ALTER TABLE `orderdetails`
+  ADD CONSTRAINT `orderdetails_ibfk_1` FOREIGN KEY (`OrderID`) REFERENCES `order` (`OrderID`),
+  ADD CONSTRAINT `orderdetails_ibfk_2` FOREIGN KEY (`ProductID`) REFERENCES `product` (`ProductID`);
+
+--
+-- Constraints for table `stock`
+--
+ALTER TABLE `stock`
+  ADD CONSTRAINT `stock_ibfk_1` FOREIGN KEY (`ShopID`) REFERENCES `shop` (`ShopID`),
+  ADD CONSTRAINT `stock_ibfk_2` FOREIGN KEY (`ProductID`) REFERENCES `product` (`ProductID`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
